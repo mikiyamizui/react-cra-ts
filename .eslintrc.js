af33efcae91d257bd1402ef1efa77dfc02d7c618
Mikiya Mizui <mikiya.mizui@gmail.com>
2023-06-20 13:05:46 +0900

    eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = /** @type {import("eslint").Linter.Config} */ ({
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    node: true,
+    browser: true,
+    es2022: true,
+    "jest/globals": true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    // "plugin:unicorn/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["!.storybook"],
+  rules: {
+    "no-console": ["warn"],
+  },
+  overrides: [
+    {
+      files: ["**/*.test.ts?(x)"],
+      extends: ["plugin:jest/recommended"],
+    },
+    {
+      files: ["**/*.stories.ts?(x)"],
+      extends: ["plugin:storybook/recommended"],
+    },
+  ],
+});
